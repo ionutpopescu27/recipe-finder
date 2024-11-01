bebce3032f4fc60c7772ee0856d7e062df17987d
Ionut Popescu <ionut272001@yahoo.ro>
2024-11-01 14:29:30 +0200

    favorites removal

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Heart } from 'lucide-react';
-import { doc, updateDoc, arrayUnion } from 'firebase/firestore';
+import { doc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
 import { FIREBASE_DB, FIREBASE_AUTH } from '../context/Firebase';
+import { useLocation } from 'react-router-dom';
 
 interface RecipeCardProps {
     title: string;
@@ -12,13 +13,17 @@ interface RecipeCardProps {
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ title, time, ingredients, instructions }) => {
 
+    const location = useLocation();
+    const isFavorite = location.pathname === '/favorites';
+    const [isDeleted, setIsDeleted] = useState<boolean>(false);
+
     const handleAddToFavorites = async (e: React.MouseEvent) => {
 
         e.stopPropagation();
 
         const user = FIREBASE_AUTH.currentUser;
         if (!user) {
-            alert("You need to log in to save favorites!");
+            alert("You need to log in to manage your favorites!");
             return;
         }
         
@@ -29,19 +34,37 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ title, time, ingredients, instr
             instructions
         };
 
-        try{    
-            const userRef = doc(FIREBASE_DB, 'users', user.uid);
+        if(!isFavorite) {
+            try{    
+                const userRef = doc(FIREBASE_DB, 'users', user.uid);
 
-           await updateDoc(userRef, {
-                favorites: arrayUnion(favoriteRecipe)
-            });
-            alert('Recipe added to favorites!');
-        }catch(error){
-            console.error('Error adding recipe to favorites:', error);
-            alert('Failed to add recipe to favorites');
+                await updateDoc(userRef, {
+                    favorites: arrayUnion(favoriteRecipe)
+                });
+                alert('Recipe added to favorites!');
+            }catch(error){
+                console.error('Error adding recipe to favorites:', error);
+                alert('Failed to add recipe to favorites');
+            }
+        } else{
+            try{    
+                const userRef = doc(FIREBASE_DB, 'users', user.uid);
+
+                await updateDoc(userRef, {
+                    favorites: arrayRemove(favoriteRecipe)
+                });
+                alert('Recipe removed from favorites!');
+                setIsDeleted(true);
+            }catch(error){
+                console.error('Error removing recipe from favorites:', error);
+                alert('Failed to remove recipe from favorites');
+            }
         }
     };
 
+    if(isDeleted) {
+        return null;
+    }
 
     return (
         <div className='bg-white rounded-lg shadow-lg p-4 flex flex-col gap-4 hover:bg-gray-200 transition-colors duration-200s'>
@@ -52,7 +75,7 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ title, time, ingredients, instr
                 </div>
                 <Heart 
                     size={24} 
-                    className='text-gray-400 hover:text-red-500 transition-colors cursor-pointer'
+                    className={`${isFavorite ? 'text-red-500 fill-current' : 'text-gray-400 hover:text-red-500 transition-colors'} cursor-pointer`}
                     onClick={handleAddToFavorites} 
                 />
             </div>
